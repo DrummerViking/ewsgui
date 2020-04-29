66743125247107c0e6fa9e6390864c1c9f4cd40e
Agustin Gallegos <agallego@microsoft.com>
2020-04-29 19:41:27 -0300

    updating changes

---
+++ internal/functions/Connect-EWSService.ps1
@@ -0,0 +1,129 @@
+﻿Function Connect-EWSService {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .EXAMPLE
+    An example
+    
+    #>
+
+    # Choosing if connection is to Office 365 or an Exchange on-premises
+    $PremiseForm.Controls.Add($radiobutton1)
+    $PremiseForm.Controls.Add($radiobutton2)
+    $PremiseForm.Controls.Add($radiobutton3)
+    $PremiseForm.Controls.Add($radiobutton4)
+    $PremiseForm.ClientSize = New-Object System.Drawing.Size(250, 160)
+    $PremiseForm.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
+    $PremiseForm.Name = "form1"
+    $PremiseForm.Text = "Choose your Exchange version"
+    #
+    # radiobutton1
+    #
+    $radiobutton1.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
+    $radiobutton1.Location = New-Object System.Drawing.Point(20, 20)
+    $radiobutton1.Size = New-Object System.Drawing.Size(150, 25)
+    $radiobutton1.TabStop = $True
+    $radiobutton1.Text = "Exchange 2007"
+    $radioButton1.Checked = $true
+    $radiobutton1.UseVisualStyleBackColor = $True
+    #
+    # radiobutton2
+    #
+    $radiobutton2.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
+    $radiobutton2.Location = New-Object System.Drawing.Point(20, 50)
+    $radiobutton2.Size = New-Object System.Drawing.Size(150, 20)
+    $radiobutton2.TabStop = $True
+    $radiobutton2.Text = "Exchange 2010"
+    $radioButton2.Checked = $false
+    $radiobutton2.UseVisualStyleBackColor = $True
+    #
+    # radiobutton3
+    #
+    $radiobutton3.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
+    $radiobutton3.Location = New-Object System.Drawing.Point(20, 80)
+    $radiobutton3.Size = New-Object System.Drawing.Size(150, 25)
+    $radiobutton3.TabStop = $True
+    $radiobutton3.Text = "Exchange 2013/2016"
+    $radiobutton3.Checked = $false
+    $radiobutton3.UseVisualStyleBackColor = $True
+    #
+    # radiobutton4
+    #
+    $radiobutton4.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
+    $radiobutton4.Location = New-Object System.Drawing.Point(20, 110)
+    $radiobutton4.Size = New-Object System.Drawing.Size(150, 30)
+    $radiobutton4.Text = "Office365"
+    $radiobutton4.Checked = $false
+    $radiobutton4.UseVisualStyleBackColor = $True
+
+    #"Go" button
+    $buttonGo.DataBindings.DefaultDataSourceUpdateMode = 0
+    $buttonGo.ForeColor = [System.Drawing.Color]::FromArgb(255, 0, 0, 0)
+    $System_Drawing_Point = New-Object System.Drawing.Point
+    $System_Drawing_Point.X = 170
+    $System_Drawing_Point.Y = 20
+    $buttonGo.Location = $System_Drawing_Point
+    $buttonGo.Name = "Go"
+    $System_Drawing_Size = New-Object System.Drawing.Size
+    $System_Drawing_Size.Height = 25
+    $System_Drawing_Size.Width = 50
+    $buttonGo.Size = $System_Drawing_Size
+    $buttonGo.Text = "Go"
+    $buttonGo.UseVisualStyleBackColor = $True
+    $buttonGo.add_Click( {
+            if ($radiobutton1.Checked) { $Global:option = "Exchange2007_SP1" }
+            elseif ($radiobutton2.Checked) { $Global:option = "Exchange2010_SP2" }
+            elseif ($radiobutton3.Checked) { $Global:option = "Exchange2013_SP1" }
+            elseif ($radiobutton4.Checked) { $Global:option = "Exchange2013_SP1" }
+            $PremiseForm.Hide()
+        })
+    $PremiseForm.Controls.Add($buttonGo)
+
+    #"Exit" button
+    $buttonExit.DataBindings.DefaultDataSourceUpdateMode = 0
+    $buttonExit.ForeColor = [System.Drawing.Color]::FromArgb(255, 0, 0, 0)
+    $System_Drawing_Point = New-Object System.Drawing.Point
+    $System_Drawing_Point.X = 170
+    $System_Drawing_Point.Y = 50
+    $buttonExit.Location = $System_Drawing_Point
+    $buttonExit.Name = "Exit"
+    $System_Drawing_Size = New-Object System.Drawing.Size
+    $System_Drawing_Size.Height = 25
+    $System_Drawing_Size.Width = 50
+    $buttonExit.Size = $System_Drawing_Size
+    $buttonExit.Text = "Exit"
+    $buttonExit.UseVisualStyleBackColor = $True
+    $buttonExit.add_Click( { $PremiseForm.Close() ; $buttonExit.Dispose() })
+    $PremiseForm.Controls.Add($buttonExit)
+
+    #Show Form
+    $PremiseForm.Add_Shown( { $PremiseForm.Activate() })
+    $PremiseForm.ShowDialog() | Out-Null
+    #exit if 'Exit' button is pushed
+    if ($buttonExit.IsDisposed) { return } 
+
+    #creating service object
+    $ExchangeVersion = [Microsoft.Exchange.WebServices.Data.ExchangeVersion]::$option
+    $service = New-Object Microsoft.Exchange.WebServices.Data.ExchangeService($ExchangeVersion)
+ 
+    #setting credentials
+    $psCred = Get-Credential -Message "Type your credentials or Administrator credentials"
+    $Global:email = $psCred.UserName
+    $authenticationContext = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext("https://login.microsoftonline.com/$TenantId", $False)
+    $platformParameters = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters([Microsoft.IdentityModel.Clients.ActiveDirectory.PromptBehavior]::Always)
+    $redirectUri = New-Object Uri("http://localhost/code")
+    $AADCredential = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserCredential" -ArgumentList $psCred.UserName, $psCred.Password
+    $authenticationResult = $authenticationContext.AcquireTokenAsync("https://outlook.office365.com", $AppDetails.AppId, $AADCredential)
+    if ($authenticationResult.Exception.InnerException.ErrorCode -eq 'interaction_required' ) {
+        $authenticationResult = $authenticationContext.AcquireTokenAsync("https://outlook.office365.com", $AppDetails.AppId, $redirectUri, $platformParameters)
+    }
+    $exchangeCredentials = New-Object Microsoft.Exchange.WebServices.Data.OAuthCredentials($authenticationResult.Result.AccessToken)
+    $Service.Credentials =  $exchangeCredentials
+    $service.Url = New-Object Uri("https://outlook.office365.com/ews/exchange.asmx")
+
+    return $service
+}
---
+++ internal/functions/Register-EWSGuiApp.ps1
@@ -0,0 +1,78 @@
+﻿Function Register-EWSGuiApp {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .EXAMPLE
+    An example
+    
+    #>
+    Invoke-PSFProtectedCommand -Action "Connecting to AzureAD" -Target "AzureAD" -ScriptBlock {
+        Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to AzureAD"
+        if ( !(Get-Module AzureAD -ListAvailable) -and !(Get-Module AzureAD) ) {
+            Install-Module AzureAD -Force -ErrorAction Stop
+        }
+        try {
+            Import-module AzureAD
+            Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] We will connect to AzureAD to allow this app to connect to your tenant using OAUTH"
+            $ConnStatus = Connect-AzureAD -Credential (Get-credential -Message "Enter Global Admin credentials") -ErrorAction Stop
+        }
+        catch {
+            if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
+                Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to Azure AD. Requesting to authenticate"
+                $ConnStatus = Connect-AzureAD -ErrorAction Stop
+            }
+            else {
+                return $_
+            }
+        }
+    } -EnableException $true -PSCmdlet $PSCmdlet
+
+    # register "PowerShellEWSScripts" as Enterprise App, by creating servicePrincipal (if not created)
+    Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Register `"PowerShellEWSScripts`" as Enterprise App, by creating servicePrincipal (if not created)"
+    if ( $null -eq ( Get-AzureADServicePrincipal -All:$True | Where-object { $_.displayname -eq "PowerShellEWSScripts" } ) ) {
+        $AzureADServicePrincipalParams = @{
+            AccountEnabled            = $True
+            AppId                     = "8799ab60-ace5-4bda-b31f-621c9f6668db"
+            ServicePrincipalNames     = "8799ab60-ace5-4bda-b31f-621c9f6668db"
+            AppRoleAssignmentRequired = $False
+            DisplayName               = "PowerShellEWSScripts"
+            PublisherName             = "Microsoft"
+            ReplyUrls                 = "http://localhost/code"
+            ServicePrincipalType      = "Application"
+            Tags                      = "WindowsAzureActiveDirectoryIntegratedApp"
+            ErrorAction               = "Stop"
+        }
+        $AppDetails = New-AzureADServicePrincipal @AzureADServicePrincipalParams
+    }
+    else {
+        $AppDetails = Get-AzureADServicePrincipal -All:$True | Where-object { $_.displayname -eq "PowerShellEWSScripts" }
+    }
+
+    # register Service Principal Assignment between Global admin and the registered app:
+    $AdminObjectId = (Get-AzureAdUser -Filter "userprincipalname eq '$($ConnStatus.Account.id)'").ObjectId
+    if ( -not (Get-AzureADServiceAppRoleAssignment -ObjectId $AppDetails.ObjectId -All:$true | Where-Object { $_.PrincipalId -eq $AdminObjectId }) ) {
+        $AppRoleAssignment = $appDetails | New-AzureADServiceAppRoleAssignment -PrincipalId $AdminObjectId -ResourceId $AppDetails.ObjectId -id "00000000-0000-0000-0000-000000000000" -ErrorAction Stop
+    }
+
+    # Grant consent to the App to access EXO and Windows Azure AD to sign in on behalf of the whole tenant (no user consent will be needed afterwards)
+    Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Grant consent to the App to access EXO and AzureAD to sign-in on behalf of the users"
+    if ( -not (Get-AzureADServicePrincipalOAuth2PermissionGrant -ObjectId $AppDetails.ObjectId -All:$True | Where-Object { ($_.Scope -eq "User.Read" -and $_.ResourceID -eq "525065a3-df7d-474d-be08-90fa1d62d4bb") -or ($_.Scope -eq "EWS.AccessAsUser.All" -and $_.ResourceID -eq "8b951d63-7dd0-46e8-a326-15e3f6a26353") }) ) {
+        $TenantId = (Get-AzureADTenantDetail).objectid
+        $context = Get-AzContext
+        $refreshToken = @($context.TokenCache.ReadItems() | Where-Object { $_.tenantId -eq $tenantId -and $_.ExpiresOn -gt (Get-Date) })[0].RefreshToken
+        $body = "grant_type=refresh_token&refresh_token=$($refreshToken)&resource=74658136-14ec-4630-ad9b-26e160ff0fc6"
+        $apiToken = Invoke-RestMethod "https://login.windows.net/$tenantId/oauth2/token" -Method POST -Body $body -ContentType 'application/x-www-form-urlencoded' -ErrorAction Stop
+        $header = @{
+            'Authorization'          = 'Bearer ' + $apiToken.access_token
+            'X-Requested-With'       = 'XMLHttpRequest'
+            'x-ms-client-request-id' = [guid]::NewGuid()
+            'x-ms-correlation-id'    = [guid]::NewGuid()
+        }
+        $url = "https://main.iam.ad.ext.azure.com/api/RegisteredApplications/$($AppDetails.AppId)/Consent?onBehalfOfAll=true"
+        Invoke-RestMethod –Uri $url –Headers $header –Method POST -ErrorAction Stop
+    }
+}
