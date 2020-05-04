914ae6a92e0a8c45ad2dec88f8d634b7b9b657ec
Agustin Gallegos <agallego@microsoft.com>
2020-05-04 18:52:02 -0300

    updating changelog

--- functions/Start-EWSGui.ps1
+++ functions/Start-EWSGui.ps1
@@ -27,16 +27,6 @@
     PS C:\ Start-Ewsgui.ps1
     Runs the GUI tool to use EWS with Exchange server and Online.
 
-    .NOTES
-        Version History:
-        1.00 - 01/30/2018 - Project start
-        1.00 - 02/19/2018 - First Release
-        1.70 - 03/13/2018 - Added new method "remove OWA Configurations"
-                            Added $service.HttpHeaders.Add("X-AnchorMailbox", $TargetSmtpAddress) , for better Impersonation performance
-                            Removed AutodiscoverSCPLookup, as most of the uses of this app is with Exchange Online
-        1.80 - 04/16/2018 - Optimized logon options. If we choose 'Office 365', we will not use SCP and we hard-code EXO endpoint.
-        1.82 - 02/15/2019 - Added 2 columns to folder lists methods 1-6 : TotalItemsCount, # of Subfolders
-        2.00 - 04/29/2020 - Moving tool to Module in GitHub
     #>
     $script:nl = "`r`n"
     $ProgressPreference = "SilentlyContinue"
